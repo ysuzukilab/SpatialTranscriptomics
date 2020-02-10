05c62023cf48ddac65882fa523949dbdea205154
Issei Tsuchiya <isseiinchicago@gmail.com>
2020-02-10 12:20:03 +0900

    modified count script

--- spatialcount.sh
+++ spatialcount.sh
@@ -16,7 +16,7 @@ COMMENT
 
 export PATH=~/visium/tools/spaceranger-1.0.0:$PATH
 spaceranger count --id=Mouse_Kidney_No4 \
-									--transcriptome=~/visium/data/refdata-cellranger-mm10-3.0.0/ \
+									--transcriptome=~/visium/reference/refdata-cellranger-mm10-3.0.0/ \
 									--fastqs=~/visium/data/fastq/200124_mouse_kidney/ \
 									--image=~/visium/data/fig/20200124124703-4-1.tif \
 									--slide=V19T12-008 \
